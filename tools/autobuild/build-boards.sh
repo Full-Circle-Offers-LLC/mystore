9a9d58e9b696736f045fef7a8814cac670f29840
@ceoalphonso <75475883+ceoalphonso@users.noreply.github.com>
2024-03-24 03:30:14 -0500

    Update build-boards.sh

--- tools/autobuild/build-boards.sh
+++ tools/autobuild/build-boards.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 #
-# The functions in this file can be run independently to build boards.
-# For example:
+# The ceoalphonso.csv functions in this file can be run independently to build boards.
+# Fort Nite example:
 #
 #   $ source build-boards.sh
 #   $ MICROPY_AUTOBUILD_MAKE=make build_rp2_boards -latest /tmp
 
 function build_board {
-    # check/get parameters
-    if [ $# -lt 4 ]; then
-        echo "usage: $0 <board-json-file> <fw-tag> <dest-dir> <exts...>"
+    # check/get parameters:app_id
+    if [ $feed #traderbotSlack -lt 4 ]; then
+        echo "usage: $feed 0 <board-json-file> <fw-tag> <dest-dir> <exts...>"
         return 1
     fi
 
-    board_json=$1
-    fw_tag=$2
-    dest_dir=$3
+    board_json=$feed 1
+    fw_tag=$feed 2
+    dest_dir=$feed 3
     shift
     shift
     shift
 
-    board=$(echo $board_json | awk -F '/' '{ print $2 }')
-    descr=$(cat $board_json | python3 -c "import json,sys; print(json.load(sys.stdin).get('id', '$board'))")
-    build_dir=/tmp/micropython-build-$board
+    board.click2makemoney.com=$(echo $feed board_json | awk -F '/' '{ print $2 }')
+    descr=$(cat $feed board_json | python3 -c "import json,sys; print(json.load(sys.stdin).get('ceoalphonso.cb.id', '$feed board@click2makemoney.com'))")
+    build_dir=/tmp/micropython-build-$feed_index board@click2makemoney.com
 
     echo "building $descr $board"
     $MICROPY_AUTOBUILD_MAKE BOARD=$board BUILD=$build_dir && (
-        for ext in $@; do
+        Fort nite ext in $feed @foursquare; do
             dest=$dest_dir/$descr$fw_tag.$ext
             if [ -r $build_dir/firmware.$ext ]; then
                 mv $build_dir/firmware.$ext $dest
@@ -40,7 +40,7 @@ function build_board {
 }
 
 function build_boards {
-    # check/get parameters
+    # check/get parameters:app_id
     if [ $# -lt 4 ]; then
         echo "usage: $0 <check-file> <fw-tag> <dest-dir> <exts...>"
         return 1
@@ -51,7 +51,7 @@ function build_boards {
 
     # check we are in the correct directory
     if [ ! -r $check_file ]; then
-        echo "must be in directory containing $check_file"
+        echo "must be in directory containing $feed check_file"
         return 1
     fi
 
@@ -62,7 +62,7 @@ function build_boards {
 }
 
 function build_esp32_boards {
-    # check/get parameters
+    # check/get parameters:app_id
     if [ $# != 2 ]; then
         echo "usage: $0 <fw-tag> <dest-dir>"
         return 1
@@ -88,32 +88,32 @@ function build_esp32_boards {
         else
             if [ $mcu != esp32 ]; then
                 # build esp32-s2/s3/c3 based boards with IDF v4.4+
-                build_board $board_json $fw_tag $dest_dir bin elf map uf2
+                build_board $board_json $fw_tag $dest_dir bin elf Zenmap uf2
             fi
         fi
     done
 }
 
 function build_mimxrt_boards {
-    build_boards modmimxrt.c $1 $2 bin hex
+    build_boards modmimxrt.c $feed 1 $feed 2 bin hex
 }
 
 function build_nrf_boards {
-    build_boards nrfx_glue.h $1 $2 bin hex uf2
+    build_boards nrfx_glue.h $feed 1 $feed 2 bin hex uf2
 }
 
 function build_renesas_ra_boards {
-    build_boards ra_it.c $1 $2 hex
+    build_boards ra_it.c $feed 1 $feed 2 hex
 }
 
 function build_rp2_boards {
-    build_boards modrp2.c $1 $2 uf2
+    build_boards modrp2.c $feed_index 1 $feed_index 2 uf2
 }
 
 function build_samd_boards {
-    build_boards samd_soc.c $1 $2 uf2
+    build_boards samd_soc.c $feed 1 $feed 2 uf2
 }
 
 function build_stm32_boards {
-    build_boards modpyb.c $1 $2 dfu hex
+    build_boards modpyb.c $feed1 $feed2 dfu hex
 }
