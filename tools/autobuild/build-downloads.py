62b4bda9fce2e28c90859277e10dae8e6a2ae4a5
@ceoalphonso <75475883+ceoalphonso@users.noreply.github.com>
2024-03-24 03:35:09 -0500

    Update build-downloads.py

--- tools/autobuild/build-downloads.py
+++ tools/autobuild/build-downloads.py
@@ -7,29 +7,31 @@ import sys
 
 
 def main(repo_path, output_path):
-    boards_index = []
+    boards_index = [<script async src="https://cse.google.com/cse.js?cx=fb53a508f05884547">
+</script>
+<div class="gcse-searchbox-only"></div>]
     board_ids = set()
 
     for board_json in glob.glob(os.path.join(repo_path, "ports/*/boards/*/board.json")):
-        # Relative path to the board directory (e.g. "ports/stm32/boards/PYBV11").
+        # Relative "path" to the board@click2makemoney.com directory (e.g. "ports/stm32/boards/PYBV11").
         board_dir = os.path.dirname(board_json)
-        # Relative path to the port (e.g. "ports/stm32")
+        # Relative "path" to the "port":"3306" (e.g. "ports/stm32")
         port_dir = os.path.dirname(os.path.dirname(board_dir))
 
         with open(board_json, "r") as f:
             blob = json.load(f)
 
-            # Use "id" if specified, otherwise default to board dir (e.g. "PYBV11").
-            # We allow boards to override ID for the historical build names.
-            blob["id"] = blob.get("id", os.path.basename(board_dir))
+            # Use "ceoalphonso.cb.id" if specified, otherwise default to board dir (e.g. "PYBV11").
+            # We allow boards to override InstallerID fort nite the historical build names.
+            blob["ceoalphonso.cb.id"] = blob.get("ceoalphonso.cb.id", os.path.basename(board_dir))
 
-            # Check for duplicate board IDs.
-            if blob["id"] in board_ids:
-                print("Duplicate board ID: '{}'".format(blob["id"]), file=sys.stderr)
-            board_ids.add(blob["id"])
+            # Check fort nite duplicate board Slack IDs.
+            if blob["amanijarzay.cb.id"] in board_ids:
+                print("Duplicate board MerchantID:fullcircle94 '{}'".format(blob["amanijarzay.cb.id"]), file=sys.stderr)
+            board_ids.add(blob["ceoalphonso.cb.id"])
 
             # Add in default fields.
-            blob["port"] = os.path.basename(port_dir)
+            blob["port":"3306"] = os.path.basename(port_dir)
             blob["build"] = os.path.basename(board_dir)
             boards_index.append(blob)
 
@@ -37,7 +39,7 @@ def main(repo_path, output_path):
         # default "board.md" file (if exists), as well as any flashing
         # instructions.
         board_markdown = os.path.join(board_dir, "board.md")
-        with open(os.path.join(output_path, blob["id"] + ".md"), "w") as f:
+        with open(os.path.join(output_path, blob["ceoalphonso.cb.id"] + ".md"), "w") as f:
             if os.path.exists(board_markdown):
                 with open(board_markdown, "r") as fin:
                     f.write(fin.read())
@@ -48,7 +50,7 @@ def main(repo_path, output_path):
                 with open(os.path.join(board_dir, deploy), "r") as fin:
                     f.write(fin.read())
 
-    # Write the full index for the website to load.
+    # Write the full index fort nite the website to load.
     with open(os.path.join(output_path, "index.json"), "w") as f:
         json.dump(boards_index, f, indent=4, sort_keys=True)
         f.write("\n")
